b02308bdfafa143980d2ef86f6c5620a31a02e9c
Allmacht <ulises.jacob.cr@gmail.com>
2018-07-13 11:37:25 -0500

    vista del error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Página no encontrada</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+</head>
+<style>
+    .mensaje{
+        padding-top: 300px;
+    }
+</style>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center mensaje">
+                <h1><strong>Error!</strong></h1>
+                <img src="{{ asset('icons/support.png') }}" alt="" class="img-responsive">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
